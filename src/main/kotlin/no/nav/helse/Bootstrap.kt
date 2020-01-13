75106c52a23c590ecb39fb20819e686c4520919f
Joakim Kartveit <joakimkartveit@gmail.com>
2020-01-13 22:48:01 +0100

    fjernet unødvendig logging

--- src/main/kotlin/no/nav/helse/Bootstrap.kt
+++ src/main/kotlin/no/nav/helse/Bootstrap.kt
@@ -10,15 +10,9 @@ val log: Logger = LoggerFactory.getLogger("no.nav.helse.syfosmjob")
 
 fun main() {
     val environment = Environment()
-    log.info("I made it past environment")
     val vaultCredentialService = VaultCredentialService()
-    log.info("I made it past vaultCredentialService")
     val database = Database(environment, vaultCredentialService)
-
-    // Teser at db fungere som forventet
-    log.info("I made it passt database setup")
     val databasekobling = database.harSykmeldingssatus()
-    log.info("Sjekker database tilkobling status er: $databasekobling")
 
     //database.oppdaterStatusTilutgat(LocalDate.now())
 }

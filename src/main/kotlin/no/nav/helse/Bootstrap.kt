8d691a75761d08f824bc0e5043aabf831dcd3bad
Joakim Kartveit <joakimkartveit@gmail.com>
2020-01-16 15:39:20 +0100

    Skrur av å opprette UTGATT statuser, er noe bug med sqlen

--- src/main/kotlin/no/nav/helse/Bootstrap.kt
+++ src/main/kotlin/no/nav/helse/Bootstrap.kt
@@ -3,7 +3,6 @@ package no.nav.helse
 import java.time.LocalDateTime
 import no.nav.helse.db.Database
 import no.nav.helse.db.VaultCredentialService
-import no.nav.helse.utgattsykmelding.registerSykmeldingerSomSkalSettesTilStatusUtgatt
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -13,12 +12,14 @@ fun main() {
     val environment = Environment()
     val vaultCredentialService = VaultCredentialService()
     val database = Database(environment, vaultCredentialService)
-    val sykmeldingerSomSkalSettesTilStatusUtgatt = database.registerSykmeldingerSomSkalSettesTilStatusUtgatt(finnUtgaatDato())
+    log.info("Hello from syfosmjob")
+    /*val sykmeldingerSomSkalSettesTilStatusUtgatt = database.registerSykmeldingerSomSkalSettesTilStatusUtgatt(finnUtgaatDato())
     if (sykmeldingerSomSkalSettesTilStatusUtgatt > 0) {
         log.info("Antall sykmeldinger som har fått status UTGATT: {}", sykmeldingerSomSkalSettesTilStatusUtgatt)
     } else {
         log.info("Ingen sykmeldinger har fått status UTGATT")
     }
+     */
 }
 
 fun finnUtgaatDato(): LocalDateTime =
